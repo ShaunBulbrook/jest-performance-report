05f33b9f118e2c3d211452fa0acb8e02d1eba0fe
Shaun Bulbrook <shaun@shaunbulbrook.com>
2023-05-31 21:34:43 +0100

    feat: Add missing test example for test report

--- src/ReportService/models/TestReport/TestReport.test.ts
+++ src/ReportService/models/TestReport/TestReport.test.ts
@@ -1,12 +1,14 @@
-import { TestResult } from "../../ReportService";
 import { TestReport } from "./TestReport";
 
 describe("TestReport", () => {
-  describe("create test report", () => {
-    test("The current time is added to the report", () => {
-      const time = new Date(0);
-      const testReport = new TestReport(time, []);
+  describe("Empty test report", () => {
+    const time = new Date(0);
+    const testReport = new TestReport(time, []);
+    test("Current time is added to the report", () => {
       expect(testReport.startTime).toBe(time);
+    });
+    test("Empty set of results", () => {
+      expect(testReport.testResults).toHaveLength(0);
     });
   });
 });
